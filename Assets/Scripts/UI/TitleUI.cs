c534bac207db2fe4dd4d36a0edb1996a700e1b8d
usernameseung <128764411+usernameseung@users.noreply.github.com>
2025-03-26 13:47:00 +0900

    코드 업뎃
    탐사 시작 버튼 클릭시 나오는 팝업ㅇㅇ

--- Assets/Scripts/UI/TitleUI.cs
+++ Assets/Scripts/UI/TitleUI.cs
@@ -10,6 +10,15 @@ public class TitleUI : MonoBehaviour
     [Tooltip("게임을 시작하는 버튼입니다.")]
     [SerializeField] private Button startButton;
 
+
+    [Header("게임 시작 확인 팝업")]
+    [Tooltip("게임 시작 버튼을 눌렀을 때 나오는 팝업창입니다.")]
+    [SerializeField] private GameObject gameStart_YesNo;
+
+    [SerializeField] private Button yesButton;
+    [SerializeField] private Button noButton;
+
+
     #endregion
 
     #region Unity Methods
@@ -23,6 +32,19 @@ public class TitleUI : MonoBehaviour
         }
 
         startButton.onClick.AddListener(OnClickStart);
+
+        // 팝업 비활성화로 시작
+        if (gameStart_YesNo != null)
+            gameStart_YesNo.SetActive(false);
+
+        // 버튼 이벤트 연결
+        if (yesButton != null)
+            yesButton.onClick.AddListener(OnClickYes);
+        Log.Info("탐험 시작 버튼 클릭");
+
+        if (noButton != null)
+            noButton.onClick.AddListener(OnClickNo);
+        Log.Info("돌아가기 버튼 클릭");
     }
 
     #endregion
@@ -30,11 +52,23 @@ public class TitleUI : MonoBehaviour
     #region Custom Methods
 
     /// <summary>
-    /// 게임 시작 버튼 클릭 시 GameScene으로 전환합니다.
+    /// 게임 시작 버튼 클릭 시 결정 팝업창이 띄워지고 GameScene으로 전환합니다.
     /// </summary>
     private void OnClickStart()
     {
+        if (gameStart_YesNo != null)
+            gameStart_YesNo.SetActive(true);
+    }
+
+    private void OnClickYes()
+    {
         SceneLoader.LoadScene(GameConstants.GAME_SCENE_NAME);
+    }
+
+    private void OnClickNo()
+    {
+        if (gameStart_YesNo != null)
+            gameStart_YesNo.SetActive(false);
     }
 
     #endregion
